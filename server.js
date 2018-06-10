f5c938e1fefeaee8188a578d24c27b35e94aaf98
David Larson <webd3vdave@gmail.com>
2018-06-09 22:10:10 -0700

    scraping working, paths working

--- server.js
+++ server.js
@@ -34,12 +34,12 @@ mongoose.connect("mongodb://localhost/mongoheadlines");
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("http://www.politico.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    // Now, we grab every h1 that is a 'headline' class, and do the following:
+    $("h1.headline").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
